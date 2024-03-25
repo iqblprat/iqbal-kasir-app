d1f41ccffb8dcea94dbab50c7763b3736ee1fb33
iqblprat <iiqbalpratt@gmail.com>
2024-03-25 21:42:45 +0700

    detail

--- transaksi/detailTransaksi.php
+++ transaksi/detailTransaksi.php
@@ -4,20 +4,29 @@ include '../config/db.php';
 
 $id_transaksi = $_GET['id_transaksi'];
 
-$sql_detail_transaksi = "SELECT t.*, d.*, b.iqbal_nama_barang, b.iqbal_harga_barang 
+$sql_detail_transaksi = "SELECT t.*, d.*, b.iqbal_nama_barang, b.iqbal_harga_barang, t.iqbal_id_user, u.iqbal_nama 
                          FROM iqbal_tb_transaksi t 
                          INNER JOIN iqbal_tb_detail d ON t.iqbal_id_transaksi = d.iqbal_id_transaksi 
                          INNER JOIN iqbal_tb_barang b ON d.iqbal_id_barang = b.iqbal_id_barang 
+                         INNER JOIN iqbal_tb_user u ON t.iqbal_id_user = u.iqbal_id_user 
                          WHERE t.iqbal_id_transaksi = '$id_transaksi'";
 $result_detail_transaksi = mysqli_query($conn, $sql_detail_transaksi);
 
 if ($result_detail_transaksi) {
     $total_harga = 0;
     $jumlah_barang = 0;
+    $id_transaksi = "";
+    $id_user = "";
+    $tanggal_transaksi = "";
+    $nama_user = "";
     while ($row = mysqli_fetch_assoc($result_detail_transaksi)) {
         $subtotal = $row['iqbal_qty'] * $row['iqbal_harga_barang'];
         $total_harga += $subtotal;
         $jumlah_barang += $row['iqbal_qty'];
+        $id_transaksi = $row['iqbal_id_transaksi'];
+        $id_user = $row['iqbal_id_user'];
+        $tanggal_transaksi = $row['iqbal_tanggal'];
+        $nama_user = $row['iqbal_nama'];
     }
 ?>
 
@@ -34,7 +43,19 @@ if ($result_detail_transaksi) {
     <div class="container mt-2">
         <h2><i class="fa-solid fa-receipt"></i> Detail Transaksi</h2>
         <hr>
-        <a href="transaksi.php" class="btn btn-outline-secondary my-2 disable"><i class="fa-solid fa-arrow-left"></i> Kembali</a>
+        <div class="row">
+            <div class="col-md-6">
+                <a href="transaksi.php" class="btn btn-outline-secondary my-2"><i class="fa-solid fa-arrow-left"></i> Kembali</a>
+            </div>
+            <div class="col-md-6">
+                <a href="cetakStrukTransaksi.php" class="btn btn-outline-dark my-2 float-end"><i class="fa-solid fa-receipt"></i> Cetak Struk Transaksi</a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-4 text-start"><b>ID Transaksi:</b> <?= $id_transaksi ?></div>
+            <div class="col-md-4 text-center"><b>User:</b> <?= $nama_user ?></div>
+            <div class="col-md-4 text-end"><b>Tanggal Transaksi:</b> <?= $tanggal_transaksi ?></div>
+        </div>
         <table class="table table-hover">
             <thead>
                 <tr>
